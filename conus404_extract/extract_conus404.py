18c28f0b2d3c48c8ea28407ccd20fca68dbcc73e
dgketchum <dgketchum@gmail.com>
2024-11-14 09:05:13 -0700

    remove repo storage of .csv, add batch shell script

--- conus404_extract/extract_conus404.py
+++ conus404_extract/extract_conus404.py
@@ -5,9 +5,9 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import xoak
-import dask
-from dask.diagnostics import ProgressBar
-from dask.distributed import Client, LocalCluster
+# import dask
+# from dask.diagnostics import ProgressBar
+# from dask.distributed import Client, LocalCluster
 
 
 def extract_conus404(stations, nc_data, out_data, workers=8, overwrite=False, bounds=None,
@@ -30,22 +30,16 @@ def extract_conus404(stations, nc_data, out_data, workers=8, overwrite=False, bo
 
     print(f'{len(station_list)} stations to write')
 
-    print(station_list[station_list.index.duplicated()])
-
-    station_list = station_list.head(n=100)
     station_list = station_list.to_xarray()
     fids = np.unique(station_list.fid.values).tolist()
-
     dates = [(year, month, calendar.monthrange(year, month)[-1])
-             for year in range(1990, 2020) for month in range(1, 13)]
-    variables = ['T2', 'TD2', 'QVAPOR', 'U10', 'V10', 'PSFC', 'ACSWDNLSM']
+             for year in range(2000, 2024) for month in range(1, 13)]
 
+    variables = ['T2', 'TD2', 'QVAPOR', 'U10', 'V10', 'PSFC', 'ACSWDNLSM']
     ds = xr.open_zarr(nc_data, consolidated=True)
     ds = ds[variables]
-
     ds.xoak.set_index(['lat', 'lon'], 'sklearn_geo_balltree')
-    with ProgressBar(), dask.config.set(scheduler='processes'):
-        ds = ds.xoak.sel(lat=station_list.latitude, lon=station_list.longitude)
+    ds = ds.xoak.sel(lat=station_list.latitude, lon=station_list.longitude)
     ds = xr.merge([station_list, ds])
 
     if debug:
@@ -54,7 +48,6 @@ def extract_conus404(stations, nc_data, out_data, workers=8, overwrite=False, bo
         return
     else:
         with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
-
             futures = [executor.submit(get_month_met, ds, fids, dt, out_data, overwrite)
                        for dt in dates]
             concurrent.futures.wait(futures)
@@ -62,22 +55,27 @@ def extract_conus404(stations, nc_data, out_data, workers=8, overwrite=False, bo
 
 def get_month_met(ds_subset, fids, date_, out_data, overwrite):
     year, month, month_end = date_
+    date_string = '{}-{}'.format(year, str(month).rjust(2, '0'))
+    print(f'selecting data from {date_string}')
     ds_subset = ds_subset.sel(time=slice(f'{year}-{month}-01', f'{year}-{month}-{month_end}'))
-    date_string = f'{year}{month}'
     all_df = ds_subset.to_dataframe()
-    for fid in fids:
-
-        dst_dir = os.path.join(out_data, fid)
-        if not os.path.exists(dst_dir):
-            os.mkdir(dst_dir)
-        _file = os.path.join(dst_dir, f'{fid}_{date_string}.csv')
-
-        if not os.path.exists(_file) or overwrite:
-            df_station = all_df.loc[slice(fid), slice(None)].copy()
-            df_station = df_station.groupby(df_station.index.get_level_values('time')).first()
-            df_station['dt'] = [i.strftime('%Y%m%d%H') for i in df_station.index]
-            df_station.to_csv(_file, index=False)
-            print(os.path.basename(_file))
+    print(f'write {date_string} from dataframe...')
+    try:
+        ct = 0
+        for fid in fids:
+            dst_dir = os.path.join(out_data, fid)
+            if not os.path.exists(dst_dir):
+                os.mkdir(dst_dir)
+            _file = os.path.join(dst_dir, f'{fid}_{date_string}.parquet')
+            if not os.path.exists(_file) or overwrite:
+                df_station = all_df.loc[slice(fid), slice(None)].copy()
+                df_station = df_station.groupby(df_station.index.get_level_values('time')).first()
+                df_station['dt'] = [i.strftime('%Y%m%d%H') for i in df_station.index]
+                df_station.to_parquet(_file, index=False)
+        if ct % 1000 == 0.:
+            print(f'{ct} for {date_string}')
+    except Exception as exc:
+        print(f'{date_string}: {exc}')
 
 
 if __name__ == '__main__':
@@ -90,10 +88,10 @@ if __name__ == '__main__':
     zarr_store = os.path.join(r, 'impd/hytest/conus404/conus404_hourly.zarr')
 
     # sites = ghcn_CANUSA_stations_mgrs.csv'
-    sites = 'madis_29OCT2024.csv'
+    sites = os.path.join(dads, 'met', 'stations', 'madis_29OCT2024.csv')
 
     csv_files = os.path.join(c404, 'station_data')
     p_files = '/data/ssd2/nldas2/parquet/'
-    extract_conus404(sites, zarr_store, csv_files, workers=24, debug=False)
+    extract_conus404(sites, zarr_store, csv_files, workers=12, debug=False)
 
 # ========================= EOF ====================================================================
